29f886e7b93fff0c8ae6ca51f6fcba8cce9db1e6
Hasitha Gallella <hbgallella@gmail.com>
2024-02-28 01:29:22 +0530

    Little modification

--- Codes/ESP32-Windows-Web-Interfacing/Server_2_comm.py
+++ Codes/ESP32-Windows-Web-Interfacing/Server_2_comm.py
@@ -1,10 +1,11 @@
 import socket
+import time
 
 def start_client(host, port):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((host, port))
 
-    message = "Data Entry Tests!"
+    message = input("Enter message: ")
     client_socket.send(message.encode('utf-8'))
 
     response = client_socket.recv(1024).decode('utf-8')
@@ -15,4 +16,6 @@ def start_client(host, port):
 if __name__ == "__main__":
     host = "127.0.0.1"
     port = 12345
-    start_client(host, port)
+    while True:
+        start_client(host, port)
+        time.sleep(0.500)
